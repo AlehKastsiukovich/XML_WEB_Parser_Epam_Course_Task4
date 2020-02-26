38d28cf85c2a7c1724beb73fb6ea312041e9077c
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-02-26 23:39:49 +0300

    Fix enum types in ToristVouchers.xml file

---
+++ src/main/java/by/epam/javatraining/xmlandwebparser/entity/TouristVouchers.java
@@ -0,0 +1,8 @@
+package by.epam.javatraining.xmlandwebparser.entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TouristVouchers {
+    private List<TouristVoucher> touristVoucherList = new ArrayList<>();
+}
---
+++ src/main/java/by/epam/javatraining/xmlandwebparser/parser/TouristVouchersDOMBuilder.java
@@ -0,0 +1,8 @@
+package by.epam.javatraining.xmlandwebparser.parser;
+
+import by.epam.javatraining.xmlandwebparser.entity.TouristVoucher;
+import java.util.List;
+
+public class TouristVouchersDOMBuilder {
+    private List<TouristVoucher> travelVoucherList;
+}
