1e9b00f0a2c332137c3e08025de2ff69efcd9f48
Aleh Kastsiukovich <alehkastsiukovich@gmail.com>
2020-03-08 20:57:13 +0300

    Add logger to StaxParseCommand.class

--- src/main/java/by/epam/javatraining/xmlandwebparser/command/parsecommand/StaxParseCommand.java
+++ src/main/java/by/epam/javatraining/xmlandwebparser/command/parsecommand/StaxParseCommand.java
@@ -6,6 +6,8 @@ import by.epam.javatraining.xmlandwebparser.entity.TouristVoucher;
 import by.epam.javatraining.xmlandwebparser.service.AbstractTouristVoucherBuilder;
 import by.epam.javatraining.xmlandwebparser.service.ParserXML;
 import by.epam.javatraining.xmlandwebparser.service.sax.TouristVouchersSAXBuilder;
+import org.apache.log4j.Logger;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -14,6 +16,7 @@ import java.text.ParseException;
 import java.util.Set;
 
 public class StaxParseCommand implements Command {
+    private static final Logger logger = Logger.getLogger(StaxParseCommand.class);
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) {
@@ -23,6 +26,7 @@ public class StaxParseCommand implements Command {
             request.setAttribute("resultSet", voucherSet);
             return PageType.PARSE_RESULT_PAGE.getValue();
         } catch (IOException | ServletException | ParseException e) {
+            logger.error(e);
             return PageType.ERROR_PAGE.getValue();
         }
     }
